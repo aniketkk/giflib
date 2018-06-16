2cd1344632cdb0b180d79beee530b0943c120f70
aniketkk <aniketrulz18@gmail.com>
2018-06-15 22:32:16 -0700

    added cross orgin allow

--- src/main/java/com/teamtreehouse/giflib/controller/CodeNamesController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/CodeNamesController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//@CrossOrigin(origins = "https://codenames-react.herokuapp.com")
+@CrossOrigin(origins = "https://codenames-react.herokuapp.com")
 @RestController
 public class CodeNamesController {
 
